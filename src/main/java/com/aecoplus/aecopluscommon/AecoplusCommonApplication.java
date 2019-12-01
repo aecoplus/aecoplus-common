ffedfa17f230eaea698ce204cd785cc17cfbbdbe
aecoplus <jxl0126@163.com>
2019-12-01 11:55:20 +0800

    init

---
+++ src/main/java/com/aecoplus/aecopluscommon/AecoplusCommonApplication.java
@@ -0,0 +1,13 @@
+package com.aecoplus.aecopluscommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AecoplusCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AecoplusCommonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aecoplus/aecopluscommon/AecoplusCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aecoplus.aecopluscommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AecoplusCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
